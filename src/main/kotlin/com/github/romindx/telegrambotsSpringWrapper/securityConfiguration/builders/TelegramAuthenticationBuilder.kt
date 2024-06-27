db709b46ae8dcb3ebd7994e9819e76413b7e6cf2
Roman Sukhorukov <romanh@rarus.ru>
2024-06-27 15:23:21 +0300

    add closure handlers

--- src/main/kotlin/com/github/romindx/telegrambotsSpringWrapper/securityConfiguration/builders/TelegramAuthenticationBuilder.kt
+++ src/main/kotlin/com/github/romindx/telegrambotsSpringWrapper/securityConfiguration/builders/TelegramAuthenticationBuilder.kt
@@ -1,5 +1,6 @@
 package com.github.romindx.telegrambotsSpringWrapper.securityConfiguration.builders
 
+import com.github.romindx.telegrambotsSpringWrapper.authentication.TelegramAuthentication
 import com.github.romindx.telegrambotsSpringWrapper.authentication.TelegramAuthenticationFilter
 import com.github.romindx.telegrambotsSpringWrapper.authentication.handlers.SuccessValidationHandler
 import com.github.romindx.telegrambotsSpringWrapper.authentication.handlers.TelegramBotTokenResolver
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.SecurityConfigurer
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
+import org.springframework.security.core.Authentication
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.util.matcher.RequestMatcher
@@ -31,8 +33,25 @@ class TelegramAuthenticationBuilder<H : HttpSecurityBuilder<H>?>()
     fun tokenResolver(resolver: TelegramBotTokenResolver) =
         this.also { it.tokenResolver = resolver }
 
+    fun tokenResolver(resolveHandler: (TelegramAuthentication) -> String) =
+        this.also {
+            it.tokenResolver = object: TelegramBotTokenResolver {
+                override fun resolve(authentication: TelegramAuthentication): String =
+                    resolveHandler(authentication)
+            }
+        }
+
     fun onSuccessValidation(handler: SuccessValidationHandler) =
         this.also { it.successValidationHandler = handler }
+
+    fun onSuccessValidation(successHandler: (TelegramAuthentication)-> Authentication) =
+        this.also {
+            it.successValidationHandler = object: SuccessValidationHandler {
+                override fun onSuccessValidation(authentication: TelegramAuthentication): Authentication =
+                    successHandler(authentication)
+
+            }
+        }
 }
 
 fun HttpSecurity.telegramAuthentication(pattern: String): TelegramAuthenticationBuilder<HttpSecurity>
--- src/main/kotlin/com/github/romindx/telegrambotsSpringWrapper/securityConfiguration/dsl/TelegramAuthenticationDsl.kt
+++ src/main/kotlin/com/github/romindx/telegrambotsSpringWrapper/securityConfiguration/dsl/TelegramAuthenticationDsl.kt
@@ -1,11 +1,13 @@
 package com.github.romindx.telegrambotsSpringWrapper.securityConfiguration.dsl
 
+import com.github.romindx.telegrambotsSpringWrapper.authentication.TelegramAuthentication
 import com.github.romindx.telegrambotsSpringWrapper.authentication.TelegramAuthenticationFilter
 import com.github.romindx.telegrambotsSpringWrapper.authentication.handlers.SuccessValidationHandler
 import com.github.romindx.telegrambotsSpringWrapper.authentication.handlers.TelegramBotTokenResolver
 import com.github.romindx.telegrambotsSpringWrapper.securityConfiguration.builders.TelegramAuthenticationBuilder
 import org.springframework.security.config.annotation.web.HttpSecurityDsl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.core.Authentication
 import org.springframework.security.web.util.matcher.RequestMatcher
 
 class TelegramAuthenticationDsl {
@@ -27,6 +29,20 @@ class TelegramAuthenticationDsl {
 
     fun onSuccessValidation(handler: SuccessValidationHandler) {
         this.successValidationHandler = handler
+    }
+
+    fun tokenResolver(resolveHandler: (TelegramAuthentication) -> String) {
+        this.tokenResolver = object: TelegramBotTokenResolver {
+            override fun resolve(authentication: TelegramAuthentication): String =
+                resolveHandler(authentication)
+        }
+    }
+
+    fun onSuccessValidation(handler: (TelegramAuthentication)-> Authentication) {
+        this.successValidationHandler = object: SuccessValidationHandler {
+            override fun onSuccessValidation(authentication: TelegramAuthentication): Authentication =
+                handler(authentication)
+        }
     }
 
     internal fun get(): TelegramAuthenticationBuilder<HttpSecurity>.() -> Unit =
